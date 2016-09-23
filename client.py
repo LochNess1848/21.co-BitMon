b9532a313b7924d25f5d67eca7bf20876b6a5d7b
LochNess1848 <Gumball176176@gmail.com>
2016-09-23 15:56:43 -0700

    Update client.py

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ wallet = Wallet()
 requests = BitTransferRequests(wallet)
 
 srv = "http://21.browntech.space:8383/"
-print("Welcome to BitMon Client!")
+print("Welcome to BitMon Client! :)")
 answer = int(input("Would you like to login[1] or register[2]?: "))
 ipaddr = get('https://api.ipify.org').text
 
